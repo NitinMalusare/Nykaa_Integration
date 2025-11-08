8791589818bc67a61014f9d9b14295658d66985d
NitinMalusare <malusarenitin404@gmail.com>
2025-11-08 18:33:49 +0530

    Remove the  unwanted data

--- src/main/java/com/eshop/auth/service/NyProductService.java
+++ src/main/java/com/eshop/auth/service/NyProductService.java
@@ -3,7 +3,6 @@ package com.eshop.auth.service;
 import com.eshop.auth.dto.NyProductCreateRequestDto;
 import com.eshop.auth.dto.NyProductRequestDto;
 import com.eshop.auth.dto.NyProductResponseDto;
-import jakarta.validation.Valid;
 
 public interface NyProductService {
     NyProductResponseDto createProducts(NyProductCreateRequestDto requestDto, String token);
